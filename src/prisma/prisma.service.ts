dcc09371a2a0656f137322d03e0c9944b8551ad9
hayoungjoe <hy.joe@enb-solution.com>
2026-02-19 16:31:42 +0900

    fix: PostgreSQL 연결 안되는 문제 수정
    - moduleFormat = "cjs" 설정
    - Prisma7 부터는 ESM 기반, Nest는 cjs 기반이기 때문에 PrismaClientInitializationError 에러 발생
    - 다른 파일에서 inject에서 사용할 경우 각 도메인 module에서 export 설정 해주어야 함.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
@@ -6,8 +7,22 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const url = process.env.DATABASE_URL;
+    if (!url) throw new Error('DATABASE_URL is not set');
+
+    const adapter = new PrismaPg({ connectionString: url });
+    super({ adapter, log: ['info', 'warn', 'error'] });
+  }
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      await this.$queryRaw`SELECT 1`;
+      console.log('✅ Prisma connected to PG');
+    } catch (error) {
+      console.error('❌ Prisma connection error:', error);
+      throw error;
+    }
   }
 
   async onModuleDestroy() {
