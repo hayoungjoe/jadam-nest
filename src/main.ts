7fe74992f7838ac48cb1cae22400e8fc21b837ca
hayoungjoe <hy.joe@enb-solution.com>
2026-02-19 16:11:18 +0900

    feat: PrismaService 생성 및 Postgresql 연결로직 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //DTO에 없는 데이터는 자동으로 삭제
+      forbidNonWhitelisted: true, //whitelist와 같이 사용했을때, 허용되지 않은 필드를 에러로 거부함. 삭제 대신 에러처리용
+      transform: true, //요청 바디 값을 DTO 타입으로 변환해줌 ex.string -> number
+    }),
+  );
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
   console.log(`${PORT}번 포트 연결 성공`);
